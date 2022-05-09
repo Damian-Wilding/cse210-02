e0b3604ecd1a867a88406814c30f9ef3bec521c7
Reed Cannon <reed.cannon97@gmail.com>
2022-05-09 10:44:08 -0600

    Dealer outline

---
+++ game/Dealer.cs
@@ -0,0 +1,28 @@
+
+using System;
+
+
+namespace Hilo
+{
+    /// <summary>
+    /// </summary> 
+    public class Dealer
+    {
+
+
+        /// <summary>
+
+        /// </summary>
+        public Dealer()
+        {
+        }
+
+        /// <summary>
+        /// </summary>
+        public void Roll()
+        {
+        
+        }
+
+    }
+}
