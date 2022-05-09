ea919e5b97036176425aefc440b7c68ef22b68a6
ckcomin <com20009@byui.edu>
2022-05-09 10:39:43 -0600

    DIrector outline

--- game/Director.cs
+++ game/Director.cs
@@ -2,6 +2,41 @@ using System;
 using System.Collections.Generic;
 
 
-namespace Hilo {
+namespace Unit02.Game
+{
+    /// <summary>
+    /// A person who directs the game. 
+    ///
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </summary>
+    public class Director
+    {
 
+        public Director()
+        {
+           
+        }
+
+        public void StartGame()
+        {
+
+        }
+
+        public void GetInputs()
+        {
+
+        }
+
+
+        public void DoUpdates()
+        {
+
+        }
+
+
+        public void DoOutputs()
+        {
+
+        }
+    }
 }
