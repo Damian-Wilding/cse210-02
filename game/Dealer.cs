3d421a2f8ed8e19d813c5d73a421fef402614c12
= <wildman700@gmail.com>
2022-05-11 10:09:33 -0600

    mmm

--- game/Dealer.cs
+++ game/Dealer.cs
@@ -5,13 +5,14 @@ using System;
 namespace Hilo
 {
     /// <summary>
+    //this is a card game bro
     /// </summary> 
     public class Dealer
     {
 
 
         /// <summary>
-
+        // I don't think anything goes here
         /// </summary>
         public Dealer()
         {
