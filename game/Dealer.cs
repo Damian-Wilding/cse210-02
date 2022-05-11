d5116fcf5403d60d36052f7e49ec197cf9019ca3
Reed Cannon <reed.cannon97@gmail.com>
2022-05-11 10:59:48 -0600

    Created 2 methods

--- game/Dealer.cs
+++ game/Dealer.cs
@@ -10,7 +10,9 @@ namespace Hilo
     public class Dealer
     {
 
-
+        int value = 0;
+        
+        int points = 0;
         /// <summary>
         // I don't think anything goes here
         /// </summary>
@@ -23,7 +25,33 @@ namespace Hilo
         /// </summary>
         public void Draw()
         {
-        
+            Random random = new Random();
+            value = random.Next(1, 14);
+        }
+
+        public int CompareCards(int previousCard, int currentCard, string playerChoice)
+        {
+
+            if (currentCard > previousCard && playerChoice == "h")
+            {
+                points = 100;
+            }
+            else if (currentCard > previousCard && playerChoice == "l")
+            {
+                points = -75;
+            }
+
+            if (currentCard < previousCard && playerChoice == "h")
+            {
+                points = -75;
+            }
+            else if (currentCard < previousCard && playerChoice == "l")
+            {
+                points = 100;
+            }
+
+            return points;
+
         }
 
     }
